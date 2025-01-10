6c6a595740717e7aa2a26a7a151ce4e6b50807ae
AJ-Stilez <adenusijoseph0@gmail.com>
2025-01-10 15:39:20 +0100

    Allowed for localhost:5174

--- server.js
+++ server.js
@@ -43,7 +43,73 @@ app.post("/upload", upload.single("logo"), multarUpload);
 
 app.post("/signUpRec", upload.single("logo"), signUpRec);
 
-app.post("/signUpCan", upload.single("resume"), signUpCan);
+app.post("/signUpCan", upload.single("resume"), async (req, res) => {
+    try{
+        const { fullName, username, email, password, jobTitle, experience, skills, roles, qualifications, jobLocation, workType, salary, availability } = req.body;
+
+        // check if candidate email exists in the database
+        const checkEmail = await CanModel.findOne({
+            email: email,
+        });
+
+        console.log(req.file);
+        
+        // check if recruiter username exists in the database
+        const checkUsername = await CanModel.findOne({
+            username: username,
+        });
+
+        // if either the email or username exitsts in the database return error
+        if(checkEmail || checkUsername){
+            throw new Error("The username or email has been used");
+        }
+
+        // if email or username does not exists in the database 
+        else{
+
+            // // // if there is no image file return error
+            if (!req.file) {
+                throw new Error('No file uploaded.');
+                }
+                
+            //   if there is an image file, then upload it and return error if any
+            const savedResume = await cloudinary.uploader.upload(req.file.path, (error, result) => {
+                
+                if(error) throw new Error(error.message);
+            });
+
+            // hash password
+            const hashedPassword = await bcrypt.hash(password, saltRounds);    
+            
+            // add candidate into the database
+            const candidate = await CanModel.create({
+                fullName: fullName,
+                username: username,
+                email: email,
+                password: hashedPassword,
+                jobTitle: jobTitle,
+                experience: experience,
+                skills: skills,
+                roles: roles,
+                resume: savedResume.url,
+                qualifications: qualifications,
+                jobLocation: jobLocation,
+                workType: workType,
+                availability: availability,
+                salary: salary,
+            });
+            
+        res.json(candidate);
+        }
+    }
+    catch(error){
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+    
+}
+);
 
 app.post("/signInRec", upload.single("none"), signInRec);
 
